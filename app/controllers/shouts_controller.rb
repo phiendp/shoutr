a16260ff504bfef8dafbd032de41ff6894b25f8e
Phien Pham <phien@triip.me>
2018-01-31 10:19:28 +0700

    Allow user to create new shout

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,20 @@
+class ShoutsController < ApplicationController
+  
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_options_for(shout)
+  end
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for(shout)
+    if shout.persisted?
+      { notice: "Shouted successfully!" }
+    else
+      { alert: "Could not add this shout!" }
+    end
+  end
+
+end
