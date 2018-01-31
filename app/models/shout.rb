ef6a6a84d589a2bad72562b5386228062213e866
Phien Pham <phien@triip.me>
2018-01-31 10:54:43 +0700

    Add show action for User and Shout models

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,4 +3,13 @@ class Shout < ApplicationRecord
 
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
+
+  default_scope { order(created_at: :desc ) }
+
+  delegate :username, to: :user
+
+  # def username
+  #   user.username
+  # end
+
 end
