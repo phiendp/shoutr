d21e7f22c89b4b7e5b99fe2d94d65c883591d52a
Phien Pham <phien@triip.me>
2018-02-03 20:11:02 +0700

    Add autolink functionality

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -12,6 +12,10 @@ module ShoutHelper
     end
   end
 
+  def autolink(text)
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
+  end
+
   private
 
   def email_digest(user_email)
