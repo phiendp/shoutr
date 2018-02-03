b8e9cf120b82260bf53751b8193f5949d173b363
Phien Pham <phien@triip.me>
2018-02-03 17:48:24 +0700

    Add user avatar using shout's helper

---
+++ app/helpers/shout_helper.rb
@@ -0,0 +1,10 @@
+module ShoutHelper
+  def avatar(user)
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest(user.email)}"
+    image_tag gravatar_url
+  end
+
+  def email_digest(user_email)
+    Digest::MD5.hexdigest(user_email)
+  end
+end
