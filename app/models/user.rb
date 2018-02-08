69550cb11b606e5c09d8194ba59711d365dbed72
Phien Pham <phien@triip.me>
2018-02-08 17:15:07 +0700

    Use clearer name for followed user relationship
    - followed_user_relationships instead of following_relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
   
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+  has_many :followed_user_relationships, 
+    foreign_key: :follower_id,
+    class_name: "FollowingRelationship",
+    dependent: :destroy
+  has_many :followed_users, through: :followed_user_relationships
   
   has_many :follower_relationships, 
     foreign_key: :followed_user_id,
