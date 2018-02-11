6d69f35ba896ad0337aeff3816dd1ad7151a3eb0
Phien Pham <phien@triip.me>
2018-02-11 12:31:09 +0700

    Extract hashtag's search scope to Query object

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,8 +1,6 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = params[:id]
-    @results = Shout.
-      joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-      where("text_shouts.body LIKE ?", "%#{@hashtag}%")
+    @results = ShoutSearchQuery.new(term: "#{@hashtag}").to_relation
   end
 end
